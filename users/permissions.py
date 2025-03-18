90ffcaba17066628fa72b92f5e62539c7fd1763b
Srijan Omar <srijanomar5840@gmail.com>
2025-03-18 11:21:34 +0530

    fix

--- users/permissions.py
+++
@@ -1,3 +0,0 @@
-# class IsAuthenticatedOrReadOnly(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return request.user.is_authenticated or request.method in permissions.SAFE_METHODS
